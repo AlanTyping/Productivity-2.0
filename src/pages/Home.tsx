09ee34b2454464b3b70b50050361b391a45b2bf5
unknown <alan.a.n.r.ar@gmail.com>
2023-03-21 17:34:00 -0300

    new changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,13 @@ export default function Home() {
   const [verified, setVerified] = useState<boolean>(false);
   useEffect(() => {
     const email = localStorage.getItem("email");
-    if (email === null) setVerified(false);
-    else setVerified(true);
+    if (email !== null) {
+        setVerified(true);
+    } else {
+        setVerified(false);
+    }
     console.log(email);
-    console.log(verified)
+    console.log(verified);
   }, []);
   return (
     <div>Home</div>
